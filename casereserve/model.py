d7c373f08fdd83178c91dfe49dae2161b9ae2d06
Rahul Kadam <coderk21@gmail.com>
2018-04-05 18:12:25 -0400

    Added abiltiy invoke python scripts

---
+++ casereserve/model.py
@@ -0,0 +1,4 @@
+from sklearn import preprocessing
+import numpy as np 
+
+print('Come to the dork side, we have', np.pi)
--- showcase/routes/index.js
+++ showcase/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var routes = express.Router();
 var url = require('url');
+var PythonShell = require('python-shell');
 
 routes.get('/', function(req, res){
 
@@ -8,5 +9,15 @@ routes.get('/', function(req, res){
     res.render('index');
 
 });
+
+//added a route for testing python script invocation
+routes.get('/pi', testpy);
+
+function testpy(req, res){
+    PythonShell.run('../casereserve/model.py', function(err, data){
+        if (err) res.send(err);
+        res.send(data.toString())
+    });
+}
 
 module.exports = routes;
